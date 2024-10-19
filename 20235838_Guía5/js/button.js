bf821ee9fa4eefd068b261ef54f8d208d396d013
leviuuuwwu <20235838@esen.edu.sv>
2024-10-19 10:39:22 -0600

    si

---
+++ 20235838_Guía5/js/button.js
@@ -0,0 +1,25 @@
+function aviso(){
+    alert("Bienvenido al mundo JavaScript")
+}
+
+function confirmacion(){
+    let confirmacion = confirm("¿Desea salir de la Sesión?");
+
+    alert(`Valor seleccionado ${confirmacion}`);
+}
+
+function capturarDatos() {
+    let nombre = prompt("¿Cuál es su nombre?");
+ 
+    let edad = prompt("¿Cuál es su edad?", 0);
+
+    alert(`Su nombre es ${nombre} y su edad ${edad}`);
+}
+
+function dibujarParrafo() {
+    let parrafo = prompt("Escriba la información que desea visualizar en el parrafo")
+
+    const p = document.querySelector("#idParrafo");
+    p.innerHTML = parrafo;
+};
+
