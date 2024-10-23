cab90c09cfd5d5665567b6c849d4f1b7961e9715
leviuuuwwu <20235838@esen.edu.sv>
2024-10-23 08:32:48 -0600

    recursos y primeros cambios

---
+++ 20235838_Guía6/js/registroAcademico.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //Accedemos al contenedor donde se mostrará los estudiantes
+    const containerEstudiantes = document.querySelector(
+        "#idContainerEstudiantes"
+    );
+
+    //Accedemos a cada boton por medio de la API DOM
+    const btnAddEstudiante = document.querySelector("#idBtnAgregarEstudiante");
+    const btnViewEstudiantes = document.querySelector("#idBtnMostrarEstudiantes")
+
+    //Agregamos el evento click a los botones, adicionalmente
+    //se le asigna la función que realizará la operacion
+    btnAddEstudiante.addEventListener("click", addEstudiantes);
+    btnViewEstudiantes.addEventListener("click", viewEstudiantes);
+
+    //Arreglo de forma global
+    let arrayEstudiantes = new Array();
+
+    //Creando funciones
+    function addEstudiantes(){
+        const inputCarnet = document
+            .querySelector("#inputCarnet")
+            .value.toString()
+            .toUpperCase();
+        const inputNombre = document
+            .querySelector("#inputNombre")
+            .value.toString()
+            .toUpperCase();
+        const inputApellidos = document
+            .querySelector("#inputApellidos")
+            .value.toString()
+            .toUpperCase();
+
+        if (inputCarnet != "" && inputNombre != "" && inputApellidos != "") {
+            arrayEstudiantes.push(
+                new Array(inputCarnet, inputNombre, inputApellidos)
+            );
+            alert("Se registro el nuevo estudiante");
+            //Limpiando campos del formulario
+            document.querySelector("#inputCarnet").value = "";
+            document.querySelector("#inputNombre").value = "";
+            document.querySelector("#inputApellidos").value = "";
+            document.querySelector("#inputCarnet").focus();
+        }    else {
+            alert("Faltan campos que completar");
+        }
+    }
+
+    function viewEstudiantes() {
+        //Validando que existen estudiantes registrados
+        let totalEstudiantes = arrayEstudiantes.length;
+        if (totalEstudiantes > 0) {
+            let carnet;
+            let nombres;
+            let apellidos;
+            let table = "<table class='table table-light table-striped'>";
+            table += "<thead>";
+            table += "<tr>";
+            table += "<th scope='col' style='width: 5%;'>#</th>";
+            table += "<th scope='col' style='width: 15%;'>Carnet</th>";
+            table += "<th scope='col'>Nombres</th>";
+            table += "<th scope='col'>Apellidos</th>";
+            table += "</tr>";
+            table += "</thead>";
+            table += "<tbody>";
+
+            //Utilizaremos un bucle for para reconocer el arreglo de estudiantes
+        }
+    }
+})
